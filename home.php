2c99911da8f6b63518459d2b0eb0890ec235948b
Bibeknepali <bibekhmn@gmail.com>
2023-09-18 08:59:50 +0545

    test

--- home.php
+++ home.php
@@ -186,17 +186,11 @@ body {
 }
 </style>
      
-            <div class="col-lg-10 py-1">
+<div class="col-lg-10 py-2">
             <div class="row">
                 <div class="col-md-12">
-                <div id="tourCarousel"  class="carousel slide" data-ride="carousel" data-interval="2000">
-                    <div id="carouselExampleControls" class="carousel slide " data-ride="carousel">
+                    <div id="carouselExampleControls" class="carousel slide bg-dark" data-ride="carousel">
                         <div class="carousel-inner">
-                        <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
                             <?php 
                                 $upload_path = "uploads/banner";
                                 if(is_dir(base_app.$upload_path)): 
@@ -208,9 +202,8 @@ body {
                                 $_i++;
                                     
                             ?>
-                            
                             <div class="carousel-item h-100 <?php echo $_i == 1 ? "active" : '' ?>">
-                            <a href="http://localhost/mobile_store/?p=products&c=c4ca4238a0b923820dcc509a6f75849b"><img src="<?php echo validate_image($upload_path.'/'.$img) ?>" class="d-block w-100  h-100" alt="<?php echo $img ?>"></a>
+                                <img src="<?php echo validate_image($upload_path.'/'.$img) ?>" class="d-block w-100  h-100" alt="<?php echo $img ?>">
                             </div>
                             <?php endforeach; ?>
                             <?php endif; ?>
